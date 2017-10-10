3950ded1a0d836be98c987fa9efcc68284a62dc6
Christina Amberson <christina.amberson@libertymutual.com>
2017-10-10 09:25:04 -0700

    Created Add Accounts and Calendar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { AddAccountsComponent } from './add-accounts/add-accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    AddAccountsComponent
   ],
   imports: [
     BrowserModule
