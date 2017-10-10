0ba3c6a94cda225cda8cad25cf2a9141d194121c
AlexisSConstantakos <LEXC023@GMAIL.COM>
2017-10-10 09:33:02 -0700

    initial component push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GradesComponent } from './grades/grades.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { AnnouncementsFormComponent } from './announcements-form/announcements-form.component';
+import { GradesFormComponent } from './grades-form/grades-form.component';
+import { PreferencesComponent } from './preferences/preferences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GradesComponent,
+    AnnouncementsComponent,
+    AnnouncementsFormComponent,
+    GradesFormComponent,
+    PreferencesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/grades-form/grades-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grades-form',
+  templateUrl: './grades-form.component.html',
+  styleUrls: ['./grades-form.component.css']
+})
+export class GradesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
